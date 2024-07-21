d25d9b387a7882b6290c8970c519be1fa1b72b26
Fares Gdoura <gdourafares@gmail.com>
2024-07-21 10:34:43 +0100

    base layout

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,22 @@
-<script setup></script>
-<template></template>
+<script setup>
+import Title from '@/components/Title.vue'
+import {reactive} from "vue";
+const playerX = reactive({
+  turn: false,
+  ownedPositions: [],
+})
+const playerO = reactive({
+  turn: false,
+  ownedPositions: [],
+})
+</script>
+<template>
+  <div class="w-screen h-screen">
+    <div class="px-[20vw] flex flex-col gap-4 py-10 w-full h-full">
+      <Title name="Play Tic Tac Toe"/>
+      <div class="tic-tac-toe grid-cols-3 grid grid-rows-3">
+        <div v-for="box in 9" :key="box" class="border aspect-square"></div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/Title.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+    default: ''
+  },
+  styling: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <h1
+    :class="styling"
+    class="text-3xl text-center"
+  >{{ name }}</h1>
+</template>
+
+<style scoped>
+
+</style>
